4f90160769d8f2f898269a8408e03827d11c6057
Github Workflow <no-reply@github-workflow.com>
2025-01-06 11:10:26 +0000

    Built workflow distribution files.

--- dist/src/commit.js
+++ dist/src/commit.js
@@ -22,12 +22,12 @@ const commitChanges = (branch, message, files, credentials) => {
         yield git.addConfig('author.email', credentials.email);
         core.info('> Adding files to git...');
         core.startGroup('Files:');
-        for (const file of files)
+        for (const file of files) {
+            yield git.add(file);
             core.info(file);
+        }
         core.endGroup();
         core.info(`> Pushing to ${branch ? 'branch ' + branch : 'origin'}...`);
-        for (const file of files)
-            yield git.add(file);
         yield git.commit(message, files);
         yield git.push('origin', branch);
     }));
