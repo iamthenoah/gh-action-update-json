6fe292a14677b92fb3f0b8231cf1598bd626a294
n.greff <n.greff@praxis-automation.nl>
2025-01-06 12:30:59 +0100

    added `fetch`

--- src/commit.ts
+++ src/commit.ts
@@ -10,11 +10,12 @@ export type GitCredentials = {
 
 export const commitChanges = (branch: string, message: string, files: string[], credentials: GitCredentials) => {
 	doAction('Commiting files', async core => {
-		core.info('> Setting up git profile...')
+		core.info('> Setting up git profile')
 		await git.addConfig('user.name', credentials.name)
 		await git.addConfig('user.email', credentials.email)
+		await git.fetch()
 
-		core.info('> Adding files to git...')
+		core.info('> Adding files to git')
 		core.startGroup('Files:')
 		for (const file of files) {
 			await git.add(file)
@@ -22,13 +23,13 @@ export const commitChanges = (branch: string, message: string, files: string[], 
 		}
 		core.endGroup()
 
-		core.info('> Committing changes...')
+		core.info('> Committing changes')
 		await git.commit(message, files)
 
-		core.info(`> Checking out ${branch} branch...`)
+		core.info(`> Checking out ${branch} branch`)
 		await git.checkout(branch)
 
-		core.info(`> Pushing to branch ${branch}...`)
+		core.info(`> Pushing to branch ${branch}`)
 		await git.push('origin', branch)
 	})
 }
